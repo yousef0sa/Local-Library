56113ce2bf9d5e7573263f647413b546001b8d58
Yousef <yosef0sa@gmail.com>
2024-03-17 19:39:52 +0300

    Add Newtonsoft.Json reference and new files

---
+++ class/ReadJSON.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_library
+{
+    public class items
+    {
+        public string image { get; set; }
+        public string title { get; set; }
+        public string date { get; set; }
+        public string link { get; set; }
+    }
+
+    internal class ReadJSON
+    {
+        public string filePath { get; set; }
+
+        /// <summary>
+        /// Retrieves a list of keys from a JSON file.
+        /// </summary>
+        /// <returns>A list of keys retrieved from the JSON file.</returns>
+        public List<string> GetJsonKeys()
+        {
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string json = r.ReadToEnd();
+                var jsonObject = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                return jsonObject.Keys.ToList();
+            }
+        }
+
+        /// <summary>
+        /// Retrieves a list of items from a JSON file.
+        /// </summary>
+        /// <param name="JsonKey">The key in the JSON file to retrieve items from. If empty, items from all keys will be retrieved.</param>
+        /// <returns>A list of items retrieved from the JSON file.</returns>
+        public List<items> GetJsonItems(string JsonKey = "")
+        {
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string json = r.ReadToEnd();
+                var jsonObject = JsonConvert.DeserializeObject<Dictionary<string, List<items>>>(json);
+                if (!string.IsNullOrEmpty(JsonKey) && jsonObject.ContainsKey(JsonKey))
+                {
+                    return jsonObject[JsonKey];
+                }
+                return jsonObject.Values.SelectMany(x => x).ToList();
+            }
+        }
+
+
+
+    }
+}
